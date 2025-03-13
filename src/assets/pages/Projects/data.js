699d1661e039937ed4b91c8f408748df06180604
Larissa <larissabenvenutia@gmail.com>
2025-03-13 14:04:43 -0300

    Adição

--- src/assets/pages/Projects/data.js
+++ src/assets/pages/Projects/data.js
@@ -1,24 +1,24 @@
 import PinterestClone from "../../images/projects/ClonePinterest.jpg";
-import WreckItRalphGame from "../../images/projects/GameDetonaRalph.png";
+import BrazilRestaurants from "../../images/projects/RestaurantesBrasil.png";
 import YourPlannerTwo from "../../images/projects/YourPlannerTwo.png";
 
 export const projects = [
   {
+    title: "Restaurantes do Brasil",
+    image: BrazilRestaurants,
+    link: "https://restaurantes-brasil.vercel.app/",
+    reverse: true,
+  },
+  {
     title: "Your Planner",
     image: YourPlannerTwo,
     link: "https://your-planner.vercel.app/",
-    reverse: true,
+    reverse: false,
   },
   {
     title: "Clone do Pinterest",
     image: PinterestClone,
     link: "https://github.com/larissabenvenuti/Clone-Pinterest",
-    reverse: false,
-  },
-  {
-    title: "Jogo do Detona Ralph",
-    image: WreckItRalphGame,
-    link: "https://github.com/larissabenvenuti/Game-DetonaRalph",
     reverse: true,
   },
 ];
